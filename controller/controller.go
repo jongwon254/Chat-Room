43226ef507a916550287cc2c7c03a2b01357d829
Joey Lee <jongwon254@gmail.com>
2022-08-08 22:17:01 +0200

    mongodb connection

---
+++ controller/controller.go
@@ -0,0 +1,35 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const connectionString = "mongodb+srv://jongwon254:1234@cluster0.izncidl.mongodb.net/?retryWrites=true&w=majority"
+const dbName = "chatroom"
+const colName = "messages"
+
+// db reference
+var collection *mongo.Collection
+
+func init() {
+	// client
+	clientOption := options.Client().ApplyURI(connectionString)
+
+	// connection to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOption)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("mongodb connection successful")
+
+	collection = client.Database(dbName).Collection(colName)
+
+	fmt.Println("Collection ready")
+}
---
+++ model/models.go
@@ -0,0 +1,12 @@
+package model
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Message struct {
+	ID   primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	User string             `json:"user,omitempty"`
+	Text string             `json:"text,omitempty"`
+	Date string             `json:"date,omitempty"`
+}
